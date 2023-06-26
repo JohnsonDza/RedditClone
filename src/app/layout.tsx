19c922ad610d368551a668385c7ff7731bf445d4
johnson <jdsouza2002@gmail.com>
2023-06-26 18:48:32 +0530

    set up complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
+import { cn } from "@/lib/utils"
+import Navbar from "@/components/Navbar"
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Breadit',
@@ -11,8 +16,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
+    <html
+    lang='en'
+    className={cn(
+      'bg-white text-slate-900 antialiased light',
+      inter.className
+    )}>
+    <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+      {/* <Providers> */}
+        <Navbar />
+        
+        {/* {authModal} */}
+
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+      {/* </Providers> */}
+      {/* <Toaster /> */}
+    </body>
+  </html>
   )
 }
